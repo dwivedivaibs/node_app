474f0dfb97a4ecab66d7e8c24160151086aebab5
Hitesh <hitesh.kanwar786@gmail.com>
2017-10-05 17:31:42 +0530

    update code

--- _config.js
+++ _config.js
@@ -1,40 +1,40 @@
-// var ids = {
-//   facebook: {
-//     clientID: '1976614272625790',
-//     clientSecret: '8d0c6db644a9afe5619efaf89d9b6c7b',
-//     callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/facebook/callback"
-//   },
-//   linkedin: {
-//     clientID: '81lv7ic9frlmtr',
-//     clientSecret: 'abs2l0M8tdBkgAjl',
-//     callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/linkedin/callback"
-//   },
-//   twitter: {
-//     consumerKey: 'EEvTaiajudSrBrDiD8mrqc6AJ',
-//     consumerSecret: '3G4JRstjNpuKNg2erNUGsLSEyKtkaWOEr0xPJ8MCtysvNM1icv',
-//     callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/twitter/callback"
-//   }
-// };
-
-// module.exports = ids;
-
-
 var ids = {
   facebook: {
     clientID: '1976614272625790',
     clientSecret: '8d0c6db644a9afe5619efaf89d9b6c7b',
-    callbackURL: "http://localhost:8000/auth/facebook/callback"
+    callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/facebook/callback"
   },
   linkedin: {
     clientID: '81lv7ic9frlmtr',
     clientSecret: 'abs2l0M8tdBkgAjl',
-    callbackURL: "http://localhost:8000/auth/linkedin/callback"
+    callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/linkedin/callback"
   },
   twitter: {
     consumerKey: 'EEvTaiajudSrBrDiD8mrqc6AJ',
     consumerSecret: '3G4JRstjNpuKNg2erNUGsLSEyKtkaWOEr0xPJ8MCtysvNM1icv',
-    callbackURL: "http://localhost:8000/auth/twitter/callback"
+    callbackURL: "https://ancient-everglades-45540.herokuapp.com/auth/twitter/callback"
   }
 };
 
 module.exports = ids;
+
+
+// var ids = {
+//   facebook: {
+//     clientID: '1976614272625790',
+//     clientSecret: '8d0c6db644a9afe5619efaf89d9b6c7b',
+//     callbackURL: "http://localhost:8000/auth/facebook/callback"
+//   },
+//   linkedin: {
+//     clientID: '81lv7ic9frlmtr',
+//     clientSecret: 'abs2l0M8tdBkgAjl',
+//     callbackURL: "http://localhost:8000/auth/linkedin/callback"
+//   },
+//   twitter: {
+//     consumerKey: 'EEvTaiajudSrBrDiD8mrqc6AJ',
+//     consumerSecret: '3G4JRstjNpuKNg2erNUGsLSEyKtkaWOEr0xPJ8MCtysvNM1icv',
+//     callbackURL: "http://localhost:8000/auth/twitter/callback"
+//   }
+// };
+
+// module.exports = ids;
